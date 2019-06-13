a6c2bedbae94bd95a1fdb52d690037aa0bd6fdab
Gregory Maddra <bloxgate@gmail.com>
2019-06-12 21:36:05 -0500

    Implement communication with GitHub

--- src/main/kotlin/BuildBot.kt
+++ src/main/kotlin/BuildBot.kt
@@ -4,6 +4,7 @@ import com.jcraft.jsch.Session
 import io.ktor.application.call
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
+import io.ktor.request.header
 import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.response.respondText
@@ -15,14 +16,17 @@ import io.ktor.server.netty.Netty
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import org.apache.commons.codec.digest.HmacAlgorithms
+import org.apache.commons.codec.digest.HmacUtils
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.lib.PersonIdent
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
-import org.eclipse.jgit.transport.JschConfigSessionFactory
-import org.eclipse.jgit.transport.OpenSshConfig
-import org.eclipse.jgit.transport.SshTransport
+import org.eclipse.jgit.transport.*
 import org.eclipse.jgit.util.FS
 import java.io.File
+import java.security.MessageDigest
+
+val WEBHOOK_KEY: String = System.getProperty("bot.webhook")
 
 val SshSessionFactory = object : JschConfigSessionFactory() {
 
@@ -40,20 +44,29 @@ val SshSessionFactory = object : JschConfigSessionFactory() {
 
 }
 
+data class PullBase(val ref: String)
 data class PullRequest(val state: String, val body: String, val merged: Boolean)
-data class PullRequestEvent(val action: String, val number: Int, val pull_request: PullRequest)
+data class PullRequestEvent(val action: String, val number: Int, val pull_request: PullRequest, val base: PullBase)
 
 fun generateChangelog(repoPath: String) {
     val command = ProcessBuilder().command("python", "$repoPath/tools/GenerateChangelog/ss13_genchangelog.py", "$repoPath/html/changelog.html", "$repoPath/html/changelogs")
     command.start().waitFor()
 }
 
+fun hmacMessage(body: String): String {
+    return "sha1=${HmacUtils(HmacAlgorithms.HMAC_SHA_1, WEBHOOK_KEY).hmacHex(body)}"
+}
+
 fun main(args: Array<String>) {
     val hsRepo = FileRepositoryBuilder.create(File("repo/.git"))
-    val hsGit: Git
+    val hsConfig = hsRepo.config
+    hsConfig.setString("core", null, "autocrlf", "true")
+    hsConfig.save()
+
+    var hsGit: Git
 
     if(hsRepo.refDatabase.findRef("HEAD") == null) {
-        Git.cloneRepository()
+        hsGit = Git.cloneRepository()
             .setURI("git@github.com:HaloSpaceStation/HaloSpaceStation13.git")
             .setDirectory(File("repo/"))
             .setGitDir(File("repo/.git"))
@@ -64,17 +77,29 @@ fun main(args: Array<String>) {
             .setBranch("refs/heads/alpha").call()
     }
     hsGit = Git(hsRepo)
+    hsGit.remoteAdd().setName("origin").setUri(URIish("git@github.com:HaloSpaceStation/HaloSpaceStation13.git")).call()
 
 
     println("Git started... starting webserver...")
-    embeddedServer(Netty, 80) {
+    embeddedServer(Netty,2703) {
         routing {
             get("/health") {
                 call.respondText("OK", ContentType.Text.Plain)
             }
 
             post("/pull") {
+                val hubSignature = call.request.header("X-Hub-Signature") ?: ""
                 val data = call.receive<String>()
+
+                val dataSignature = hmacMessage(data)
+                if(MessageDigest.isEqual(dataSignature.toByteArray(), hubSignature.toByteArray()))
+                {
+                    call.respond(HttpStatusCode.OK, "")
+                } else {
+                    call.respond(HttpStatusCode.Unauthorized, "Invalid Hub Signature")
+                    return@post
+                }
+
                 val jsonData = Klaxon().parse<PullRequestEvent>(data)
                 if(jsonData == null)
                 {
@@ -82,7 +107,7 @@ fun main(args: Array<String>) {
                     return@post
                 }
 
-                if(jsonData.action == "closed" && jsonData.pull_request.merged)
+                if(jsonData.action == "closed" && jsonData.pull_request.merged && jsonData.base.ref == "alpha")
                 {
                     val body = jsonData.pull_request.body
                     var clText = body.substring(body.indexOf("\uD83C\uDD91 ") + 3)
@@ -109,21 +134,41 @@ fun main(args: Array<String>) {
                         clFile.writeText(clYaml)
 
                         withContext(Dispatchers.IO) {
+                            hsGit.pull()
+                                .setRemote("origin")
+                                .setTransportConfigCallback {
+                                    (it as SshTransport).sshSessionFactory = SshSessionFactory
+                                }
+                                .call()
+
                             runBlocking {
                                 val repoPath = File("repo/").absolutePath
                                 generateChangelog(repoPath)
                             }
 
-                            hsGit.add().addFilepattern("html").call()
+                            hsGit.add().addFilepattern("html").setUpdate(true).call()
                             val commit = hsGit.commit()
                             commit.author = PersonIdent("HaloStation Build Bot", "<hs13@ss13.projectunsc.org>")
-                            commit.message = "Autogenerated Changelog for PR #${jsonData.number}"
+                            commit.message = "[ci skip] Autogenerated Changelog for PR #${jsonData.number}"
                             commit.call()
+
+                            println("Changes committed... Attempting to push...")
+
+                            val push = hsGit.push()
+                            push.refSpecs = listOf(RefSpec("refs/heads/alpha:refs/heads/alpha"))
+                            push.setTransportConfigCallback {
+                                    (it as SshTransport).sshSessionFactory = SshSessionFactory
+                            }
+                            try {
+                                push.call()
+                            } catch (ex: Exception) {
+                                println(ex.message)
+                            }
+
+                            println("Push complete!")
                         }
                     }
                 }
-
-                call.respond(HttpStatusCode.OK, "")
             }
         }
     }.start(wait = true)
