7d9b36d536722ae6c191b8f0570c1d4d6b950e89
Gregory Maddra <bloxgate@gmail.com>
2019-11-11 19:14:01 -0600

    Make changelog generation thread-safe

--- src/main/kotlin/BuildBot.kt
+++ src/main/kotlin/BuildBot.kt
@@ -28,6 +28,7 @@ import org.eclipse.jgit.transport.*
 import org.eclipse.jgit.util.FS
 import java.io.File
 import java.security.MessageDigest
+import java.util.concurrent.locks.ReentrantLock
 
 val WEBHOOK_KEY: String = System.getProperty("bot.webhook")
 
@@ -47,12 +48,20 @@ val SshSessionFactory = object : JschConfigSessionFactory() {
 
 }
 
+// Only let 1 CL be written at a time
+val changelogLock = ReentrantLock()
+
 data class PullBase(val ref: String)
 data class PullRequest(val state: String, val body: String, val base: PullBase, val merged: Boolean)
 data class PullRequestEvent(val action: String, val pull_request: PullRequest, val number: Int)
 
 fun generateChangelog(repoPath: String) {
-    val command = ProcessBuilder().command("python", "$repoPath/tools/GenerateChangelog/ss13_genchangelog.py", "$repoPath/html/changelog.html", "$repoPath/html/changelogs")
+    val command = ProcessBuilder().command(
+        "python",
+        "$repoPath/tools/GenerateChangelog/ss13_genchangelog.py",
+        "$repoPath/html/changelog.html",
+        "$repoPath/html/changelogs"
+    )
     command.start().waitFor()
 }
 
@@ -76,7 +85,7 @@ fun main(args: Array<String>) {
     val hsRepo = FileRepositoryBuilder.create(File("repo/.git"))
     var hsGit: Git
 
-    if(hsRepo.refDatabase.findRef("HEAD") == null) {
+    if (hsRepo.refDatabase.findRef("HEAD") == null) {
         hsGit = Git.cloneRepository()
             .setURI("git@github.com:HaloSpaceStation/HaloSpaceStation13.git")
             .setDirectory(File("repo/"))
@@ -95,7 +104,7 @@ fun main(args: Array<String>) {
     hsConfig.save()
 
     println("Git started... starting webserver...")
-    embeddedServer(Netty,2703) {
+    embeddedServer(Netty, 2703) {
         routing {
             get("/health") {
                 call.respondText("OK", ContentType.Text.Plain)
@@ -106,8 +115,7 @@ fun main(args: Array<String>) {
                 val data = call.receive<ByteArray>().toString(Charsets.UTF_8)
 
                 val dataSignature = hmacMessage(data)
-                if(MessageDigest.isEqual(dataSignature.toByteArray(), hubSignature.toByteArray()))
-                {
+                if (MessageDigest.isEqual(dataSignature.toByteArray(), hubSignature.toByteArray())) {
                     println("Valid request!")
                     call.respond(HttpStatusCode.OK, "")
                 } else {
@@ -118,14 +126,12 @@ fun main(args: Array<String>) {
                 println("Parsing JSON...")
                 val jsonData = Klaxon().parse<PullRequestEvent>(data)
                 println("Parsed!")
-                if(jsonData == null)
-                {
+                if (jsonData == null) {
                     call.respond(HttpStatusCode.BadRequest, "Bad Request!")
                     return@post
                 }
-		
-                if(jsonData.action == "closed" && jsonData.pull_request.merged && jsonData.pull_request.base.ref == "alpha")
-                {
+
+                if (jsonData.action == "closed" && jsonData.pull_request.merged && jsonData.pull_request.base.ref == "alpha") {
                     println("Parsing PR ${jsonData.number}")
                     val body = jsonData.pull_request.body
 
@@ -134,8 +140,7 @@ fun main(args: Array<String>) {
                     val author = clListener.author
                     val logEntries = clListener.entries
 
-                    if (logEntries.isNotEmpty() && author != "")
-                    {
+                    if (logEntries.isNotEmpty() && author != "") {
                         println("PR ${jsonData.number} was parsed")
                         var clYaml = """
                             author: $author
@@ -145,44 +150,49 @@ fun main(args: Array<String>) {
                         logEntries.forEach { (key, value) ->
                             clYaml += "\n  - $key: \"${value.replace(Regex("[^\\p{L}\\p{Z}\\p{N},.]"), "")}\""
                         }
+                        changelogLock.lock()
                         val clFile = File("repo/html/changelogs/pr-${jsonData.number}-changes.yml")
-                        clFile.writeText(clYaml)
+                        try {
+                            clFile.writeText(clYaml)
 
-                        withContext(Dispatchers.IO) {
-                            println("Pulling...")
-                            hsGit.pull()
-                                .setRemote("origin")
-                                .setTransportConfigCallback {
+                            withContext(Dispatchers.IO) {
+                                println("Pulling...")
+                                hsGit.pull()
+                                    .setRemote("origin")
+                                    .setTransportConfigCallback {
+                                        (it as SshTransport).sshSessionFactory = SshSessionFactory
+                                    }
+                                    .call()
+
+                                runBlocking {
+                                    println("Running scripts...")
+                                    val repoPath = File("repo/").absolutePath
+                                    generateChangelog(repoPath)
+                                }
+
+                                hsGit.add().addFilepattern("html").setUpdate(true).call()
+                                val commit = hsGit.commit()
+                                commit.author = PersonIdent("HaloStation Build Bot", "<hs13@ss13.projectunsc.org>")
+                                commit.message = "[ci skip] Autogenerated Changelog for PR #${jsonData.number}"
+                                commit.call()
+
+                                println("Changes committed... Attempting to push...")
+
+                                val push = hsGit.push()
+                                push.refSpecs = listOf(RefSpec("refs/heads/alpha:refs/heads/alpha"))
+                                push.setTransportConfigCallback {
                                     (it as SshTransport).sshSessionFactory = SshSessionFactory
                                 }
-                                .call()
+                                try {
+                                    push.call()
+                                } catch (ex: Exception) {
+                                    println(ex.message)
+                                }
 
-                            runBlocking {
-                                println("Running scripts...")
-                                val repoPath = File("repo/").absolutePath
-                                generateChangelog(repoPath)
+                                println("Push complete!")
                             }
-
-                            hsGit.add().addFilepattern("html").setUpdate(true).call()
-                            val commit = hsGit.commit()
-                            commit.author = PersonIdent("HaloStation Build Bot", "<hs13@ss13.projectunsc.org>")
-                            commit.message = "[ci skip] Autogenerated Changelog for PR #${jsonData.number}"
-                            commit.call()
-
-                            println("Changes committed... Attempting to push...")
-
-                            val push = hsGit.push()
-                            push.refSpecs = listOf(RefSpec("refs/heads/alpha:refs/heads/alpha"))
-                            push.setTransportConfigCallback {
-                                    (it as SshTransport).sshSessionFactory = SshSessionFactory
-                            }
-                            try {
-                                push.call()
-                            } catch (ex: Exception) {
-                                println(ex.message)
-                            }
-
-                            println("Push complete!")
+                        } finally {
+                            changelogLock.unlock()
                         }
                     }
                 }
